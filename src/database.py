33302fe1ed0c6914423d10d002d4eb6d9c986a44
jnamgung <namgung.john@gmail.com>
2019-02-08 21:25:35 -0500

    switched computers

--- src/database.py
+++ src/database.py
@@ -1,2 +1,8 @@
 # Store and retreive information from a MySQL server
 import mysql.connector as mysql
+
+def connect_database():
+    db = mysql.connector.connect(user="server", database="")
+
+def store_entry(user_id, source_site dest_site):
+    pass
