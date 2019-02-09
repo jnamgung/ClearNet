e31301527bde2eccade0901eaf04559264812d23
jnamgung <namgung.john@gmail.com>
2019-02-09 10:51:52 -0500

    Webpage

--- src/link.py
+++ src/link.py
@@ -1,2 +1,27 @@
 # Handle all end user transactions through link
-from bottle import get
+from flask import Flask, redirect, render_template, make_response, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/check', methods = ['POST', 'GET'])
+def check():
+    if request.method == 'POST':
+        user = request.form['user']
+        return render_template('register.html', value=user)
+
+
+@app.route('/link/<out>')
+def link(out):
+    return '<h1>Hello, World! Welcome to ' + out + '!</h1>'
+
+@app.route('/redirect/<link>/<out>') 
+def redirection(link, out):
+    return redirect("https://www." + link + ".com/" + out)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
