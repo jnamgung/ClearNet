b110eb340788250a7c8cbd2f836cdbe97861800b
jnamgung <namgung.john@gmail.com>
2019-02-09 02:12:59 -0500

    Finished basic database interactions

--- src/database.py
+++ src/database.py
@@ -8,10 +8,13 @@ class Database:
         self.password = password
         self.connection = None
         self.cursor = None
+        return
 
     def connect_database(self):
-        cnx = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server 
-            + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password)
+        cnx = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=' 
+            + self.server + ';Database=' + self.database + ';Uid=' 
+            + self.username + ';Pwd=' + self.password
+            + ';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
         if self.connection is not None:
             self.connection.close()
         self.connection = cnx
@@ -24,24 +27,35 @@ class Database:
             self.connection.close()
             self.cursor = None
             self.connection = None
+        return
+    
+    def host_exists(self, host_id):
+        return self.cursor.tables(table=host_id, tableType='TABLE').fetchone()
+
+    def add_host(self, host_id):
+        if not self.host_exists(host_id):
+            self.cursor.execute("CREATE TABLE " + host_id + " (SID INTEGER, "
+                + "SSITE VARCHAR(256), DSITE VARCHAR(256), T DATETIME)")
+        self.connection.commit()
+        return
+
+    def del_host(self, host_id):
+        self.cursor.execute("DROP TABLE " + host_id)
+        self.connection.commit()
+        return
     
     def store_entry(self, host_id, session_id, source_site, dest_site, time):
-        insert = ("INSERT INTO self.database (host_id, session_id, source_site, dest_site, time) " + 
-                  "VALUES (%(host_id)s, %(session_id)s, %(source_site)s, %(dest_site)s, %(time)s)")
-        vals = {
-            'host_id': host_id,
-            'session_id': session_id,
-            'source_site': source_site,
-            'dest_site': dest_site,
-            'time': time
-            }
-        self.cursor.execute(insert, vals)
+        query = ("INSERT INTO " + host_id + "(SID, SSITE, DSITE, T) "
+            + "VALUES(?, ?, ?, ?)")
+        vals = [session_id, source_site, dest_site, time]
+        self.cursor.execute(query, *vals)
         self.connection.commit()
+        return
 
-    def read_entry(self, session_id, time):
-        query = ("SELECT (host_id, session_id, source_site, dest_site, time) " +
-                 "FROM self.database WHERE (session_id = %(session_id)s AND time=%(time)s")
-        self.cursor.execute(query, {'session_id': session_id, 'time': time})
+    def read_entry(self, host_id, session_id, time):
+        query = ("SELECT SID, SSITE, DSITE, T FROM " + host_id + " WHERE "
+            + "SID=? AND T=?")
+        self.cursor.execute(query, [session_id, time])
         rows = self.cursor.fetchall()
         return rows
 
