0ec32588e30eef682e5cfb47954978d320d25398
jnamgung <namgung.john@gmail.com>
2019-02-09 00:26:55 -0500

    database fixes

--- src/database.py
+++ src/database.py
@@ -7,23 +7,26 @@ class Database:
         self.username = username
         self.password = password
         self.connection = None
+        self.cursor = None
 
     def connect_database(self):
         cnx = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server 
             + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password)
         if self.connection is not None:
             self.connection.close()
-        self.connection = cnx.cursor()
+        self.connection = cnx
+        self.cursor = cnx.cursor()
         return
 
     def close_database(self):
-        if self.connection is not None:
+        if self.connection is not None and self.cursor is not None:
+            self.cursor.close()
             self.connection.close()
+            self.cursor = None
             self.connection = None
     
     def store_entry(self, host_id, session_id, source_site, dest_site, time):
-        cursor = cnx.cursor();
-        insert = ("INSERT INTO self.database (host_id, session_id, source_site, dest_site, time)"
+        insert = ("INSERT INTO self.database (host_id, session_id, source_site, dest_site, time) " + 
                   "VALUES (%(host_id)s, %(session_id)s, %(source_site)s, %(dest_site)s, %(time)s)")
         vals = {
             'host_id': host_id,
@@ -32,15 +35,14 @@ class Database:
             'dest_site': dest_site,
             'time': time
             }
-        cursor.execute(insert, vals)
-        cnx.commit()
+        self.cursor.execute(insert, vals)
+        self.connection.commit()
 
-    def read_entry(self, session_id, requested_time):
-        cursor = cnx.cursor();
-        query = ("SELECT (host_id, session_id, source_site, dest_site, time)"
+    def read_entry(self, session_id, time):
+        query = ("SELECT (host_id, session_id, source_site, dest_site, time) " +
                  "FROM self.database WHERE (session_id = %(session_id)s AND time=%(time)s")
-        cursor.execute(query, {'session_id': requested_tid, 'time': requested_time})
-        rows=cursor.fetchall();
+        self.cursor.execute(query, {'session_id': session_id, 'time': time})
+        rows = self.cursor.fetchall()
         return rows
 
 
